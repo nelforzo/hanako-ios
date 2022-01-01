1710918517be696cc6efccdd9a6a7261b466e415
Frank Lopez <spiccanelforzo@outlook.com>
2022-01-01 22:13:19 +0900

    Initial Commit

---
+++ Shared/hanako_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hanako_iosApp.swift
+//  Shared
+//
+//  Created by Frank Lopez on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct hanako_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
